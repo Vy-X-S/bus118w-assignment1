79d77b8f515a4e692e1a109b54aaa9ebf53e70b0
Mikhail.io <mikhail.ocampo@sjsu.edu>
2023-09-28 23:44:04 -0700

    Updates

---
+++ precache-manifest.2f7f4c9afa6bfe650725a5bb48840ee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf2a7878624e314a021",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/static/css/main.22dd8231.chunk.css"
+  },
+  {
+    "revision": "fcf2a7878624e314a021",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/static/js/main.fcf2a787.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "2a463c745e2afde5bc4e",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/static/js/runtime~main.2a463c74.js"
+  },
+  {
+    "revision": "ac1d29a1703b39767a8c1208c79b0d93",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/static/media/profile.ac1d29a1.jpeg"
+  },
+  {
+    "revision": "e6037a8100745c7a134c32f1e6b1a9f8",
+    "url": "https://Vy-X-S.github.io/bus118w-assignment1/index.html"
+  }
+];
