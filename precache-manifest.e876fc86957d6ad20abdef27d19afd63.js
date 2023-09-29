4875b5f33fa58b431548a818d52a92b3de9c84f3
Mikhail.io <mikhail.ocampo@sjsu.edu>
2023-09-28 23:29:00 -0700

    Updates

---
+++ precache-manifest.e876fc86957d6ad20abdef27d19afd63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf2a7878624e314a021",
+    "url": "/Vy-X-S/bus118w-assignment1/static/css/main.22dd8231.chunk.css"
+  },
+  {
+    "revision": "fcf2a7878624e314a021",
+    "url": "/Vy-X-S/bus118w-assignment1/static/js/main.fcf2a787.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Vy-X-S/bus118w-assignment1/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/Vy-X-S/bus118w-assignment1/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "37d834875e3dce247a98",
+    "url": "/Vy-X-S/bus118w-assignment1/static/js/runtime~main.37d83487.js"
+  },
+  {
+    "revision": "ac1d29a1703b39767a8c1208c79b0d93",
+    "url": "/Vy-X-S/bus118w-assignment1/static/media/profile.ac1d29a1.jpeg"
+  },
+  {
+    "revision": "caeca71b76a5308abec3f2af4034a54f",
+    "url": "/Vy-X-S/bus118w-assignment1/index.html"
+  }
+];
